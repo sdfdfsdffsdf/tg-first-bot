acd79698cd668c278410fa378ac845d8557746a9
sdfdfsdffsdf <141039736+sdfdfsdffsdf@users.noreply.github.com>
2023-08-15 14:44:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 api_id = 14428023
 api_hash = 'ff2ca1e060eacbfa40eb9803c49cb542'
-key = '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'
+key = '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'
